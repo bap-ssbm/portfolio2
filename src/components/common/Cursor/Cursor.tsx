76bf239396c494028bfa54e5a5c32dda674c5fe5
Oshimoto_Kentaro <oshimoto.kentaro@holon-inc.jp>
2024-03-17 00:09:55 +0900

    edit cursor

--- src/components/common/Cursor/Cursor.tsx
+++ src/components/common/Cursor/Cursor.tsx
@@ -18,8 +18,8 @@ function Cursor({className, ...props} : Props) {
     useEffect(() => {
        
         const positionElement = (e: MouseEvent) => {
-            setMouseY(e.clientY - 13);
-            setMouseX(e.clientX - 15);
+            setMouseY(e.clientY - 17);
+            setMouseX(e.clientX - 17);
         };
 
         const hoverHandler = (e: MouseEvent) => {
@@ -57,7 +57,7 @@ function Cursor({className, ...props} : Props) {
     <div className='pointer-events-none w-screen h-svh fixed top-0 left-0 bg-black mix-blend-difference z-[99999]'>
 <div ref={cursorRef} style={{ transform: `translate3d(${mouseX}px, ${mouseY}px, 0)`, display: outOfScreen?'none':'inline-block'}} id='cursor' className='duration-100 cursor pointer-events-none w-10 h-10 mix-blend-difference rounded-full relative z-50 invert pc:inline-blockhidden pc:flex items-center justify-center hidden'>
      {hovered&&<p className='relative z-50 text-black text-2xl mix-blend-difference text-center ease-in duration-100'>押</p>}
-    <div style={{ transform: `${hovered? 'scale(5)' : ''}` }}  className='curso__inner w-full h-full rounded-full invert opacity-80 bg-back absolute top-0 left-0 border border-white ease-in-out duration-300 text-sm'>   
+    <div style={{ transform: `${hovered? 'scale(4)' : ''}` }}  className='curso__inner w-full h-full rounded-full invert opacity-80 bg-back absolute top-0 left-0 border border-white ease-in-out duration-500 text-sm'>   
     </div>
   </div>
     </div>
